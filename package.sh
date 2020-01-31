79c543bc5a2851dce77532ecd0523f1d19e94558
Michael Stegeman <michael@stegeman.me>
2020-01-31 11:29:41 +0100

    Update workflows.

--- package.sh
+++ package.sh
@@ -1,13 +1,24 @@
-#!/bin/bash
+#!/bin/bash -e
 
 version=$(grep '"version":' manifest.json | cut -d: -f2 | cut -d\" -f2)
 
-rm -rf SHA256SUMS package
+rm -rf node_modules
+
+# If you have npm production dependencies, uncomment the following line
+# npm install --production
+
 mkdir package
-cp *.js manifest.json LICENSE README.md package/
-cp -r css images js views package/
+cp -r css images js views *.js manifest.json LICENSE README.md package/
+
+# If you have npm production dependencies, uncomment the following line
+# cp -r node_modules package/
+
 cd package
-find . -type f -exec sha256sum {} \; >> SHA256SUMS
+find . \( -type f -o -type l \) -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 cd ..
 
-tar czf "example-extension-${version}.tgz" package
+TARFILE="example-extension-${version}.tgz"
+tar czf ${TARFILE} package
+shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+
+rm -rf package
